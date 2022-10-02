4658d7ab8a77b772a9a544f5505fd4c608410395
Adam Kabak <adamkabak@yahoo.com>
2022-10-02 12:57:53 -0400

    cleaned up

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import Clock from "./clock";
 import ShoppingList from "./shoppingList";
 import { render } from "@testing-library/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Square from "./square.jsx";
-import Board from "./board.jsx";
+
 import Game from "./game.jsx";
 import "./index.css";
 
 const page = (
   <div className="div-class">
-    <ShoppingList />
     <Game />
   </div>
 );
